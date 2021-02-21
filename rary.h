123fb9ccd6d99eb6a243c8c7568397ae4dcb9525
g0rbe <mail@danielgorbe.com>
2021-02-21 18:46:36 +0100

    update RARY_TERM_COLOR_* color

--- rary.h
+++ rary.h
@@ -1,11 +1,11 @@
 #ifndef RARY_H
 #define RARY_H
 
-#define RARY_TERM_COLOR_RED     "\033[1;31m"
-#define RARY_TERM_COLOR_GREEN   "\033[1;32m"
-#define RARY_TERM_COLOR_YELLOW  "\033[1;33m"
-#define RARY_TERM_COLOR_BLUE    "\033[1;34m"
-#define RARY_TERM_COLOR_NOCOLOR "\033[1;0m"
+#define RARY_TERM_COLOR_RED     "\033[0;31m"
+#define RARY_TERM_COLOR_GREEN   "\033[0;32m"
+#define RARY_TERM_COLOR_YELLOW  "\033[0;33m"
+#define RARY_TERM_COLOR_BLUE    "\033[0;34m"
+#define RARY_TERM_COLOR_NOCOLOR "\033[0;0m"
 
 #include "utils.h"
 #include "systemd.h"
